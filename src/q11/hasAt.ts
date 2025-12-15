49a410c413b3d3ec1f378f09ac5af8deae9cb9c9
68010244@kmitl.ac.th <68010244@kmitl.ac.th>
2025-12-15 17:36:53 +0700

    q11

---
+++ src/q11/hasAt.ts
@@ -0,0 +1,12 @@
+const input = process.argv[2];
+
+if (input === undefined) {
+  process.exit(0);
+}
+
+for (let i = 0; i < input.length; i++) {
+  if (input[i] === "@") {
+    console.log("Has @");
+    process.exit(0);
+  }
+}
