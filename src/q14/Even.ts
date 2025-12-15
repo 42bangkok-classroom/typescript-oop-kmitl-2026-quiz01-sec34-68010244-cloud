56fb26a1eacc9e30f98d0c2015f94a891686ae56
68010244@kmitl.ac.th <68010244@kmitl.ac.th>
2025-12-15 18:07:18 +0700

    q21

--- src/q14/Even.ts
+++ src/q14/Even.ts
@@ -1,15 +1,15 @@
 const arg = process.argv[2];
+
 if (arg === undefined) {
   process.exit(0);
 }
 
-const N = Number(arg);
-
-if (Number.isNaN(N)) {
+const n = Number(arg);
+if (Number.isNaN(n)) {
   process.exit(0);
 }
 
-for (let i = 0; i <= N; i++) {
+for (let i = 0; i <= n; i++) {
   if (i % 2 === 0) {
     console.log(i);
   }
